bdd99b09a2341a0ac2b18d4834e4f2e310d1c82a
Exotic-Retard <ddoogedoge@gmail.com>
2016-02-16 21:25:49 +0000

    superior taunts 1
    ohh yes

--- taunts.py
+++ taunts.py
@@ -1,16 +1,30 @@
 TAUNTS = [
     "{name}, you are the biggest idiot i've encountered yet.",
     "Isn't your mommy here to play with you, {name}?",
+    "You should be ashamed of yourself, {name}.",
+    "{name}: You stack worse than SGI clan.",
+    "{name}: You disgust me.",
 ]
 
 USE_FORBIDDEN = [
     "We can clearly see who the idiot is. It is {name}, for trying to use me.",
     "Hey everyone do you see {name}? A true moron.",
     "The Empire will not succumb to you.",
+    "Did you really think someone of your competence would succeed in this?",
+    "Oh were you trying to use me, or just frothing at the mouth as usual?",
+    "No.",
+    "Come back when your IQ is above 30.",
+    "{name}: You disgust me.",
+    "Clearly you dont belong on a computer so just go back to your corner.",
+    "Your lack of intelligence amazes me.",
 ]
 
 TALKING_REACTION = [
     "{name} shut up.",
     "Be quiet when the big ones are talking.",
     "{name} what is wrong with you? Stay quiet.",
+    "Only the worthy may speak here, and in case it hasn't got to that thick skull of yours, that does not include you.",
+    "You are not welcome here. Now stay silent.",
+    "The door's that way {name}.",
+    "Look everyone, {name} is spewing nonsense again.",
 ]
